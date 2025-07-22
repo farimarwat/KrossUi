efc5c7d56fb018720cb2096b0a1becc4d99c5fbc
farimarwat <farimarwat@gmail.com>
2025-07-22 16:09:18 +0500

    Add minor changes

--- composeApp/src/commonMain/kotlin/com/farimarwat/krossuidemo/App.kt
+++ composeApp/src/commonMain/kotlin/com/farimarwat/krossuidemo/App.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
